6adc37938a1962089babb818ada39a68182d1a97
Diogo Almeida <diogoa@kth.se>
2017-10-11 13:59:18 +0200

    Sleep between measurements

--- force_torque_sensor_calib/src/ft_calib_node.cpp
+++ force_torque_sensor_calib/src/ft_calib_node.cpp
@@ -600,7 +600,7 @@ int main(int argc, char **argv)
 			ret = ft_calib_node.moveNextPose();
 			t_end_move_arm = ros::Time::now();
 		}
-
+    sleep(1.0);
 		// average 100 measurements to calibrate the sensor in each position
 		else if ((ros::Time::now() - t_end_move_arm).toSec() > wait_time)
 		{
